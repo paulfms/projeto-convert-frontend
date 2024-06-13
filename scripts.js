0417f5274cce0fa476af44116333d29dedb3b018
Paul Anderson <pauldevop@gmail.com>
2024-06-13 15:13:32 -0300

    Capturing currency value

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+ // Obtendo os elementos do formulário.
+ const form = document.querySelector("form")
  const amount = document.getElementById("amount")
+ const currency = document.getElementById("currency")
 
  // Manipulando o input amount para receber somente números.
  amount.addEventListener("input", () => {
@@ -6,3 +9,10 @@
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
  })
+
+ // Capturando o evento de submit (enviar) do formulário.
+ form.onsubmit = (event) => {
+    event.preventDefault()
+
+    console.log(currency.value)
+ }
