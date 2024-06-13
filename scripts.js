691b20bd3c2cc07bdd02dbc702199fcf5197e644
Paul Anderson <pauldevop@gmail.com>
2024-06-13 15:43:30 -0300

    Adding the currency quote

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -36,8 +37,12 @@ form.onsubmit = (event) => {
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol) {
     try {
+        // Exibe a cotação da moeda
+        description.textContent = `${symbol} 1 = ${price}`
+
         // Aplica a classe que exibe o footer para mostrar o resultado
         footer.classList.add("show-result")
+
     } catch (error) {
         // Remove a classe do footer removendo ele da tela.
         footer.classList.remove("show-result")
